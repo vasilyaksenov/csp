d73be58f3368c24fbcffd2c1539c73d8a375043a
Vasily Axenov <vasilyaksenov@gmail.com>
2023-08-15 20:06:49 +0300

    Add get_initial_patterns() and solve_master()

--- csp.cpp
+++ csp.cpp
@@ -1,42 +1,120 @@
 #include "csp.h"
-#include "ortools/linear_solver/linear_solver.h"
+#include <ostream>
+#include <cmath>
+#include "ortools/base/logging.h"
 
-namespace operations_research {
-
-    void BasicExample() {
-        // Create the linear solver with the GLOP backend.
-        std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("CBC_MIXED_INTEGER_PROGRAMMING"));
-
-        // Create the variables x and y.
-        MPVariable* const x = solver->MakeNumVar(0.0, 1, "x");
-        MPVariable* const y = solver->MakeNumVar(0.0, 2, "y");
-
-        LOG(INFO) << "Number of variables = " << solver->NumVariables();
-
-        // Create a linear constraint, 0 <= x + y <= 2.
-        MPConstraint* const ct = solver->MakeRowConstraint(0.0, 2.0, "ct");
-        ct->SetCoefficient(x, 1);
-        ct->SetCoefficient(y, 1);
-
-        LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
-
-        // Create the objective function, 3 * x + y.
-        MPObjective* const objective = solver->MutableObjective();
-        objective->SetCoefficient(x, 3);
-        objective->SetCoefficient(y, 1);
-        objective->SetMaximization();
-
-        solver->Solve();
-
-        LOG(INFO) << "Solution:" << std::endl;
-        LOG(INFO) << "Objective value = " << objective->Value();
-        LOG(INFO) << "x = " << x->solution_value();
-        LOG(INFO) << "y = " << y->solution_value();
+namespace csp {
+    solver::solver(std::vector<order_t>& orders_list, uint64_t blanks_width) : orders_list{ orders_list }, blanks_width_in_units{ blanks_width }
+    {
+        /* TODO: Check for empty input parameters */
     }
 
+    /*
+        Returns identity matrix of orders_list size.
+        For example if orders_list size is 3 it will be:
+        1 0 0
+        0 1 0
+        0 0 1
+     */
+    std::vector<std::vector<uint64_t>> solver::get_initial_patterns()
+    {
+        uint64_t orders_num = orders_list.size();
+        std::vector<std::vector<uint64_t>> init_patterns( /* init vector of vectors as all zeroes */
+            orders_num,
+            std::vector<uint64_t>(orders_num));
+
+        for (uint64_t i = 0; i < orders_list.size(); ++i) {
+            for (uint64_t j = 0; j < orders_list.size(); ++j) {
+                if (j == i) {
+                    init_patterns[i][j] = 1;
+                }
+            }
+        }
+
+        return init_patterns;
+    }
+
+    solver::result solver::solve_master(std::vector<std::vector<uint64_t>>& patterns)
+    {
+        /* TODO: Check for empty patterns vector */
+
+        uint64_t patterns_num = patterns[0].size(); /* Assuming the matrix is squared */
+        result ret;
+
+        std::unique_ptr<operations_research::MPSolver> solver(
+            operations_research::MPSolver::CreateSolver("GLOP_LINEAR_PROGRAMMING"));
+
+        if (!solver) {
+            LOG(WARNING) << "GLOP_LINEAR_PROGRAMMING solver unavailable.";
+            return ret;
+        }
+
+        std::vector<operations_research::MPVariable*> y;
+
+        for (int i = 0; i < patterns_num; ++i) {
+            y.push_back(solver->MakeIntVar(0.0, 1000.0, ""));
+        }
+
+        /* Create the objective - minimize total blanks used */
+        operations_research::MPObjective* const objective = solver->MutableObjective();
+
+        for (int i = 0; i < patterns_num; ++i) {
+            objective->SetCoefficient(y[i], 1);
+        }
+
+        objective->SetMinimization();
+
+        /* Add constraints that patterns (orders) must be met */
+        const double infinity = solver->infinity();
+        std::vector<operations_research::MPConstraint*> constraints;
+        for (int i = 0; i < patterns_num; ++i) {
+            constraints.push_back(solver->MakeRowConstraint(orders_list[i].num, infinity));
+            for (int j = 0; j < patterns_num; ++j) {
+                constraints.back()->SetCoefficient(y[j], patterns[i][j]);
+            }
+        }
+
+        ret.result_status = solver->Solve();
+
+        // Check that the problem has an optimal solution.
+        if (ret.result_status != operations_research::MPSolver::OPTIMAL) {
+            LOG(INFO) << "The problem does not have an optimal solution!";
+            if (ret.result_status == operations_research::MPSolver::FEASIBLE) {
+                LOG(INFO) << "A potentially suboptimal solution was found";
+            }
+            else {
+                LOG(INFO) << "The solver could not solve the problem.";
+                return ret;
+            }
+        }
+
+        for (auto& x : y) {
+            ret.solution_values.push_back((*x).solution_value());
+        }
+
+        for (auto& x : constraints) {
+            ret.dual_values.push_back((*x).dual_value());
+        }
+
+        return ret;
+    }
 }
 
 int main() {
-    operations_research::BasicExample();
+
+    std::vector<csp::order_t> orders_list {
+        { 20, 700 },
+        { 40, 400 },
+        { 32, 234 },
+        { 256, 23 },
+        { 12, 657 },
+    };
+
+    std::unique_ptr<csp::solver> solver = std::make_unique<csp::solver>(orders_list, 3000);
+
+    std::vector<std::vector<uint64_t>> initial_patterns = solver->get_initial_patterns();
+
+    solver->solve_master(initial_patterns);
+
     return EXIT_SUCCESS;
 }
--- csp.h
+++ csp.h
@@ -1,3 +1,40 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include "ortools/linear_solver/linear_solver.h"
+
+namespace csp {
+
+    /** @struct order_t
+     *  @brief Basic order structure. Consists of the width of the pieces need to cut and their
+     *  @brief number.
+     */
+    struct order_t {
+        uint64_t num;    /*!< Number of the pieces */
+        uint64_t width;   /*!< Width of the pieces */
+    };
+
+    class solver
+    {
+        public:
+            uint64_t blanks_width_in_units = 0;
+            const double blanks_width_in_percent = 100.0;
+            std::vector<order_t> orders_list; /*!< List of orders */
+
+        private:
+            struct result {
+                std::vector<double> solution_values;
+                std::vector<double> dual_values;
+                operations_research::MPSolver::ResultStatus result_status = operations_research::MPSolver::NOT_SOLVED;
+            };
+
+        public:
+            solver(std::vector<order_t>& orders_list, uint64_t blanks_width);
+
+        public:
+            std::vector<std::vector<uint64_t>> get_initial_patterns();
+            result solve_master(std::vector<std::vector<uint64_t>>& patterns);
+
+    };   
+}
